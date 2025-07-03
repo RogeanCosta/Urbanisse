52f33ea5d789399f896d45bf60b977c63b1298ec
matheus sales <156109892+Math-eus1@users.noreply.github.com>
2025-07-03 16:10:35 -0300

    criei um primeiro header

---
+++ src/Header.jsx
@@ -0,0 +1,13 @@
+import logo from './assets/Urbanisse-logo.svg'
+export default function Header(){
+
+    return (
+        <>
+        <span>
+        <h2>Urbanisse</h2>
+        <h5>Clothing Store</h5> 
+        
+        </span>
+        </>
+    )
+}
